11373de99df8a654d0001e18dffbd67002e323e5
aceh <songyaqigg@gmail.com>
2022-12-13 12:02:53 +0800

    build: 初始化项目

---
+++ DesignCode/DesignCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeApp.swift
+//  DesignCode
+//
+//  Created by 宋亚奇 on 2022/12/13.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
